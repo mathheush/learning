2795870067b4bf003e444ab3490a829daea01e51
Mateusz Konopka <124741586+mathheush@users.noreply.github.com>
2023-08-01 13:54:18 +0200

    Update prognozaPogody.py

--- prognozaPogody.py
+++ prognozaPogody.py
@@ -1,24 +1,35 @@
-import json
 import requests
 import datetime
+import random
+
 
 print("Witaj w aplikacji, która pozwoli Ci sprawdzić pogodę.\n")
 
 def main():
+    with open("yourcity.txt") as f:
+        yours_city = f.readlines()
     x = input("Wpisz \"1\", jeśli chcesz sprawdzić pogodę w danym mieście.\n"
-              "Wpisz \"2\", aby wyświetlić pogodę (aktualne miasto: xxx).\n"
+              f"Wpisz \"2\", aby wyświetlić pogodę (aktualne miasto: {yours_city[0]})\n"
               "Wpisz \"3\", żeby zobaczyć pogodę w losowym mieście.\n"
+              "Wpisz \"4\", by zmienić ustawione miasto.\n"
               "Wpisz \"q\", by zakończyć.\n"
               "Co chcesz zrobić?: "
               )
     if x == "1":
         type_city()
+        main()
 
     elif x == "2":
         deafult_city()
+        main()
 
     elif x == "3":
         random_city()
+        main()
+
+    elif x == "4":
+        set_city()
+        main()
 
     elif x == "q":
         pass
@@ -46,9 +57,9 @@ def weather():
     x = (x.strftime("%H"))
     x = int(x)
     now = weather[x]
-    print(f"{named}, aktualna temperatura wynosi {now}°C \n"
+    print(f"\n\n{named}, aktualna temperatura wynosi {now}°C \n"
           f"Najniższa dziś wyniesie {lowest}°C"
-          f", a z kolei najwyższ to {highest}°C"
+          f", a z kolei najwyższa to {highest}°C \n\n"
 
     )
 
@@ -58,6 +69,29 @@ def type_city():
     location()
     weather()
 
-main()
+def deafult_city():
+    with open("yourcity.txt") as f:
+        yours_city = f.readlines()
+    global typed_city
+    typed_city = yours_city[0]
+    location()
+    weather()
 
+def set_city():
+    change_city = input("Wpisz swoje miasto: ")
+    save_city = open("yourcity.txt", "w")
+    save_city.write(change_city)
+    save_city.close
+    print("\nTwoje miasto zostało zapisane!\n")
+
+def random_city():
+    global typed_city
+    typed_city = ["Lódź", "Gdańsk", "Wrocław", "Częstochowa", "Zielona Góra", "Kraków", "Zakopane", "Berlin"]
+    x = random.randint(-1, 7)
+    typed_city = typed_city[x]
+    location()
+    weather()
+
+
+main()
 
